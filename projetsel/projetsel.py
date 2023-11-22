122e90172f54719862aee98ad72b80da83f04def
root <root@deb12-vdi-rt>
2023-11-22 11:26:25 +0100

    Debut du code python

---
+++ projetsel/projetsel.py
@@ -0,0 +1,16 @@
+import moduleprojetsel.py
+
+etableau = pd.read_excel("../data/conductivite_amont_20211012.xls")             #Lecture du fichier excel et enregistrement des données dans la variable etableau
+
+def calcul(etableau):                                                           #Fonction de calcul du débit de la rivière à partir de la variable etableau
+    print(etableau)
+
+
+
+
+
+
+
+
+def graphique(etableau):                                                        #Fonction qui permet de générer un graphique qui va être affiché sur la page web
+    print(salut)
