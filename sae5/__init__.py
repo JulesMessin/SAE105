ca1ae6a902b23c3da82e3dbb7ef3ce13bb4f13e1
etudiant <etudiant@deb12-vdi-rt>
2023-11-27 15:56:29 +0100

    Ajout de __init__.py à l'arborescence

---
+++ sae5/__init__.py
@@ -0,0 +1,5 @@
+""" Mon projet
+.. moduleauthor:: Messin Jules <messin.jules@etu.univ-poitiers.fr>
+                  Vigner Benjamin <vigner.benjamin@etu.univ-poitiers.fr>
+"""
+import sae5_module.py
